e510814363b1a11f1dd87ead942bb60550037fd0
Fuad Mustamirrul Ishlah <fuad.mustamirrul@gmail.com>
2021-09-29 21:33:12 +0700

    Add project files.

---
+++ UTSSistemBerkas.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+void printVecInt(std::vector<int>& data) {
+    std::cout << "[";
+    for (size_t i = 0; i < data.size(); i++)
+    {
+        std::cout << data[i];
+        if (i < data.size() - 1) {
+            std::cout << ",";
+        }
+    }
+    std::cout << "]";
+}
+
+std::vector<int> insertData() {
+    size_t count;
+    std::cout << "Jumlah data: ";
+    std::cin >> count;
+    std::vector<int> data = {};
+    data.resize(count);
+    for (size_t i = 0; i < count; i++)
+    {
+        int in;
+        std::cout << "Masukkan data index ke-"<<i<<": ";
+        std::cin >> in;
+        data[i] = in;
+    }
+    return data;
+}
+
+void searchData(std::vector<int>& data) {
+    int key;
+    std::cout << "Data yang dicari: ";
+    std::cin >> key;
+    for (size_t i = 0; i < data.size(); i++)
+    {
+        if (data[i] == key) {
+            std::cout << "Data ditemukan pada index ke-"<<i;
+            return;
+        }
+    }
+    std::cout << "Data tidak ditemukan";
+}
+int main()
+{
+    bool running = true;
+    std::vector<int> data = {};
+    while (running) {
+        std::cout << "Isi data: ";
+        printVecInt(data);
+        std::cout << "\n";
+        std::cout << "Operasi yang akan dilakukan:\n";
+        std::cout << "1. Rekam data\n";
+        std::cout << "2. Cari data\n";
+        std::cout << "0. Keluar\n";
+        int op;
+        std::cin >> op;
+        switch (op)
+        {
+        case 1:
+            data = insertData();
+            break;
+        case 2:
+            searchData(data);
+            break;
+        default:
+            running = false;
+            break;
+        }
+    }
+}
